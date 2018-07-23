3cc75d612a2872d47a30a8b4eed8001a4e179f87
Corn Pivotal <cmendoza@pivotal.io>
2018-07-22 20:00:50 -0500

    Updated

---
+++ sources/AspDotNet4/CloudFoundryWeb/Controllers/AttendeesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CloudFoundryWeb.Models;
+
+namespace CloudFoundryWeb.Controllers
+{
+    public class AttendeesController : ApiController
+    {
+        private AttendeeContext db = new AttendeeContext();
+
+        // GET: api/Attendees
+        public IQueryable<AttendeeModel> GetAttendeeModels()
+        {
+            return db.AttendeeModels;
+        }
+
+        // GET: api/Attendees/5
+        [ResponseType(typeof(AttendeeModel))]
+        public IHttpActionResult GetAttendeeModel(int id)
+        {
+            AttendeeModel attendeeModel = db.AttendeeModels.Find(id);
+            if (attendeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(attendeeModel);
+        }
+
+        // PUT: api/Attendees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAttendeeModel(int id, AttendeeModel attendeeModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != attendeeModel.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(attendeeModel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttendeeModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Attendees
+        [ResponseType(typeof(AttendeeModel))]
+        public IHttpActionResult PostAttendeeModel(AttendeeModel attendeeModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AttendeeModels.Add(attendeeModel);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AttendeeModelExists(attendeeModel.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = attendeeModel.Id }, attendeeModel);
+        }
+
+        // DELETE: api/Attendees/5
+        [ResponseType(typeof(AttendeeModel))]
+        public IHttpActionResult DeleteAttendeeModel(int id)
+        {
+            AttendeeModel attendeeModel = db.AttendeeModels.Find(id);
+            if (attendeeModel == null)
+            {
+                return NotFound();
+            }
+
+            db.AttendeeModels.Remove(attendeeModel);
+            db.SaveChanges();
+
+            return Ok(attendeeModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AttendeeModelExists(int id)
+        {
+            return db.AttendeeModels.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ sources/AspDotNet4/CloudFoundryWeb/Models/AttendeeContext.cs
@@ -0,0 +1,48 @@
+﻿using Pivotal.Workshop;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CloudFoundryWeb.Models
+{
+    public class AttendeeContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AttendeeContext() : base(ConnectionString)
+        {
+        }
+
+        public static string ConnectionString
+        {
+            get
+            {
+                try
+                {
+                    CFEnvironmentVariables _env = new CFEnvironmentVariables(ApplicationConfig.Configuration);
+                    var _connect = _env.getConnectionStringForDbService("user-provided", "AttendeeContext");
+                    if (!string.IsNullOrEmpty(_connect))
+                    {
+                        Console.WriteLine($"Using UPS: '{_connect}' for connection");
+                        return _connect;
+                    }
+                }
+                catch { }
+
+                var _s = System.Configuration.ConfigurationManager.ConnectionStrings["AttendeeContext"].ConnectionString;
+                Console.WriteLine($"Using web.config: '{_s}' for connection");
+
+                return _s;
+            }
+        }
+
+        public System.Data.Entity.DbSet<CloudFoundryWeb.Models.AttendeeModel> AttendeeModels { get; set; }
+    }
+}
